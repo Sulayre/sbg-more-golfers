5ce095e57fcdc99634b647437711a20a0fc440bf
Sulayre <jose.alonso.pares@gmail.com>
2026-02-23 19:55:47 -0300

    v1.1.0

--- MoreGolfersPlugin.cs
+++ MoreGolfersPlugin.cs
@@ -20,7 +20,7 @@ public class MoreGolfersPlugin : BaseUnityPlugin
         MaxPlayersConfig = Config.Bind("General", "MaxPlayers", 32f, "Player limit");
         var harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
         harmony.PatchAll();
-
+    
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded and patches applied!");
     }
     
@@ -47,14 +47,14 @@ public static class SliderLogicPatch
                 found = true;
                 if (finds > 1)
                 {
-                    MoreGolfersPlugin.Logger.LogInfo($"[{MyPluginInfo.PLUGIN_GUID}]: patched player limit from 16 to 32.");
+                    MoreGolfersPlugin.Logger.LogInfo("Patched MatchSetupMenu delegate");
                     break;   
                 }
             }
         }
 
         if (!found)
-            MoreGolfersPlugin.Logger.LogWarning($"[{MyPluginInfo.PLUGIN_GUID}]: instruction Ldc_I4_S was not found with value 16.");
+            MoreGolfersPlugin.Logger.LogWarning("instruction Ldc_I4_S was not found with value 16.");
 
         return codes.AsEnumerable();
     }
@@ -71,13 +71,37 @@ public static class MatchSetupMenu_Patch
             if (!found && instruction.opcode == OpCodes.Ldc_R4 && (float)instruction.operand == 16f)
             {
                 instruction.opcode = OpCodes.Call;
-                instruction.operand = AccessTools.Method(typeof(MatchSetupMenu_Patch), nameof(MoreGolfersPlugin.GetCustomMaxPlayers));
+                instruction.operand = AccessTools.Method(typeof(MoreGolfersPlugin), nameof(MoreGolfersPlugin.GetCustomMaxPlayers));
                 found = true;
-                MoreGolfersPlugin.Logger.LogInfo(
-                    $"[{MyPluginInfo.PLUGIN_GUID}]: Patched onStartClient to handle increased players on room configuration.");
+                MoreGolfersPlugin.Logger.LogInfo("Patched MatchSetupMenu.onStartClient");
             }
 
             yield return instruction;
         }
+    }
+}
+
+[HarmonyPatch(typeof(TeeingPlatformSettings), "MaxTeeCount", MethodType.Getter)]
+class PatchMaxTeeCount
+{
+    static bool Prefix(ref int __result) 
+    {
+        __result = (int)MoreGolfersPlugin.MaxPlayersConfig.Value / 4;
+        // MoreGolfersPlugin.Logger.LogInfo("Patched TeeingPlatformSettings.MaxTeeCount");
+        return false; 
+    }
+}
+
+[HarmonyPatch(typeof(GolfTeeManager), "ReturnHitTeeInternal")]
+public static class PatchReturnHitTeeInternal
+{
+    private static readonly AccessTools.FieldRef<GolfTeeManager, int> MaxPoolSizeRef = 
+        AccessTools.FieldRefAccess<GolfTeeManager, int>("maxHitTeePoolSize");
+    
+    [HarmonyPrefix]
+    static void Prefix(GolfTeeManager __instance)
+    {
+        MaxPoolSizeRef(__instance) = (int)MoreGolfersPlugin.MaxPlayersConfig.Value;
+        MoreGolfersPlugin.Logger.LogInfo("Patched GolfTeeManager.maxHitTeePoolSize through GolfTeeManager.ReturnHitTeeInternal");
     }
 }
